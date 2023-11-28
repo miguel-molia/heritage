347bcbd9aafb5059f988e4d2c7bf588659e404b5
miguel-molia <miguel.molia@laplateforme.io>
2023-11-28 11:11:38 +0100

    Update: added comment

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import model.Patient;
 public class Main {
     public static void main(String[] args) {
         Patient patient = new Patient("John", "Wick");
+        //test//
         System.out.println(patient.getFirstName() + " " + patient.getLastName());
         Patient patient2 = new Patient(222, "Jeremie", "Laroche", "masculin", "1992 Avenue Trouduc", "066666666");
         System.out.println(patient2.getGender() +  " " + patient2.getId());
